d3db56faa7715d5747ce4dbe16b0c52acab17168
PhamThanhSJU <146055328+PhamThanhSJU@users.noreply.github.com>
2024-10-24 22:46:16 +0900

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -5,72 +5,103 @@ import numpy as np
 import streamlit as st
 # Information
 st.sidebar.markdown("## Authors' information")
-st.sidebar.markdown("Author: Tan-Duy Phan")
-st.sidebar.caption("Falculty of Civil Engineering-HCMUT-VNU\nEmail:\nptduy.sdh212@hcmut.edu.vn\nGoogle scholar:\nhttps://scholar.google.com/citations?user=Zi9MR6QAAAAJ&hl=en ")
+st.sidebar.markdown("Authors: Van-Thanh Pham and Jong-Sung Kim")
+st.sidebar.caption("Department of Quantum and Nuclear Engineering, Sejong University, Korea")
+st.sidebar.caption("Emails: phamthanhwru@gmail.com and kimjsbat@sejong.ac.kr")
 
 #Tên bài báo
-st.title ("Predicting the compressive strength of ultra high-performance concrete: an ensemble machine learning approach and actual application") 
+st.title ("Hybrid machine learning model using HHO algorithm for predicting the onset of void swelling in irradiated metals") 
+
 # Chèn sơ đồ nghiên cứu
-st.header("1. Layout of this study")
-check1=st.checkbox('1.1 Display layout of this investigation')
-if check1:
-   st.image("Fig. 1.jpg", caption="Layout of this study")
-# Hiển thị dữ liệu
-st.header("2. Dataset")
-check2=st.checkbox('2.1 Display dataset')
-if check2:
-   Database="Dataset.csv"
-   df = pandas.read_csv(Database)
-   df.head()
-   st.write(df)
-st.header("3. Modeling approach")
-check3=st.checkbox('3.1 Display structure of Random Forest model')
-if check3:
-   st.image("Fig2.jpg", caption="Overview on structure of Random Forest model") 
+# st.header("1. Layout of this study")
+# check1=st.checkbox('1.1 Display layout of this investigation')
+# if check1:
+#    st.image("Fig. 1.jpg", caption="Layout of this study")
+# # Hiển thị dữ liệu
+# st.header("2. Dataset")
+# check2=st.checkbox('2.1 Display dataset')
+# if check2:
+#    Database="Dataset.csv"
+#    df = pandas.read_csv(Database)
+#    df.head()
+#    st.write(df)
+# st.header("3. Modeling approach")
+# check3=st.checkbox('3.1 Display structure of Random Forest model')
+# if check3:
+#    st.image("Fig2.jpg", caption="Overview on structure of Random Forest model") 
+	
 #Make a prediction
-st.header("4. Predicting compressive strength of UHPC using the RF model")
+st.header("Predicting the onset of void swelling in irradiated metals")
 st.subheader("Input variables")
 col1, col2, col3, col4 =st.columns(4)
 with col1:
-   X1=st.slider("Cement (kg/m\u00B3)", 270, 1250)
-   X2 = st.slider("Silica fume (kg/m\u00B3)", 0, 375)
-   X3= st.slider("Slag (kg/m\u00B3)", 0, 434)
-   X4 = st.slider("Limestone powder (kg/m\u00B3)", 0, 1059)	
+   X1=st.slider("Fe (wt. %)", 0, 97.0)
+   X2 = st.slider("Cr (wt. %)", 0, 24.7)
+   X3= st.slider("Mn (wt. %)", 0, 20.0)
+   X4 = st.slider("Si (wt. %)", 0, 1.5)
+   X5 = st.slider("Co (wt. %)", 0, 0.04)
+   X6= st.slider("Mo (wt. %)", 0, 2.95)
+   X7 = st.slider("Ni (wt. %)", 0, 100.0)
+
 with col2:
-   X5 =st.slider("Quartz powder (kg/m\u00B3)",0, 397)
-   X6 =st.slider("Fly ash (kg/m\u00B3)",0, 356)
-   X7=st.slider("Nano silica (kg/m\u00B3)", 0,48)
-   X8=st.slider("Water (kg/m\u00B3)", 90,273)
+   X8=st.slider("C (wt. %)", 0, 1.0)
+   X9 = st.slider("Ti (wt. %)", 0, 2.2)
+   X10= st.slider("N (wt. %)", 0, 0.3)
+   X11 = st.slider("B (wt. %)", 0, 0.004)
+   X12 = st.slider("P (wt. %)", 0, 0.155)
+   X13= st.slider("S (wt. %)", 0, 0.03)
+   X14 = st.slider("Nb (wt. %)", 0, 0.92)   
+	
 with col3:
-   X9=st.slider("Sand (kg/m\u00B3)", 0,1503)
-   X10=st.slider("Coarse aggregate (kg/m\u00B3)", 0,1195)
-   X11=st.slider("Fiber (kg/m\u00B3)", 0,234)
-   X12=st.slider("Superplasticizer (kg/m\u00B3)", 1,57)	
+   X15=st.slider("Cu (wt. %)", 0, 0.54)
+   X16 = st.slider("Ta (wt. %)", 0, 0.36)
+   X17= st.slider("Al (wt. %)", 0, 100.0)
+   X18 = st.slider("V (wt. %)", 0, 2.0)
+   X19 = st.slider("Mg (wt. %)", 0, 1.63)
+   X20= st.slider("W (wt. %)", 0, 2.4)
+   X21 = st.slider("Zr (wt. %)", 0, 0.1)  
+
 with col4:	
-   X13=st.slider("Relative humidity (%)", 50,100)
-   X14=st.slider("Temperature (\u00B0C)", 20,210)
-   X15=st.slider("Age (day)", 1,365)
-Data="Dataset.csv"
-df = pandas.read_csv(Data)
-df.head()
-X = df.iloc[:, 0:15].values 
-y = df.iloc[:, 15].values 
-#  model
-from sklearn.ensemble import RandomForestRegressor
-RF= RandomForestRegressor(max_depth=None,
-	                       max_leaf_nodes=None,
-	                       max_samples=None,
-	                       min_samples_leaf=1,
-	                       min_samples_split=2,
-	                       min_impurity_decrease=0,
-	                       n_estimators=1000,
-	                       random_state=0)
-RF.fit(X, y)
-Inputdata = [X1, X2, X3, X4, X5, X6, X7,X8, X9, X10, X11, X12, X13, X14, X15]
+   X22=st.slider("Dose rate (dpa/s)", 0, 97.0)
+   X23 = st.slider("Temperature (K)", 0, 24.7)
+   X24= st.slider("Irradiation type", 1, 4)
+   X25 = st.slider("Dislocation density x10^14 (m\u00B(−2))", 0.3, 38.5)
+   X26 = st.slider("Pre-injected He (appm)", 0, 100.0)
+
+
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.ensemble import GradientBoostingRegressor
+from catboost import CatBoostRegressor
+
+#Model
+
+from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+df = pd.read_csv('Swelling_data_Thanh.csv')
+X_ori = df[['Fe','Cr','Mn','Si','Co','Mo','Ni','C','Ti','N','B','P','S','Nb','Cu','Ta','Al','V','Mg','W','Zr','dr','tem','irrtype','disden','PreHe']].values
+y = df['dose'].values
+X = scaler.fit_transform(X_ori)	
+X_train=X
+y_train=y
+
+n_estimators = 115
+learning_rate = 0.518
+depth = 9
+l2_leaf_reg = 9
+        
+cat_clf_n = CatBoostRegressor(n_estimators=n_estimators,learning_rate = learning_rate, depth=depth,l2_leaf_reg=l2_leaf_reg, random_state=42)
+cat_clf_n.fit(X_train, y_train)
+
+
+Inputdata1 = [X1, X2, X3, X4, X5, X6, X7,X8, X9, X10, X11, X12, X13, X14, X15, X16, X17,X18, X19, X20, X21, X22, X23, X24, X25*(10**14),X26]
+Inputdata=scaler.transform(Inputdata1)
+
 from numpy import asarray
 Newdata = asarray([Inputdata])
 print(Newdata)
-fc_pred2 = RF.predict(Newdata)
+fc_pred2 = cat_clf_n.predict(Newdata)
+
 st.subheader("Output variable")
 if st.button("Predict"):
     import streamlit as st
@@ -82,5 +113,5 @@ if st.button("Predict"):
        my_bar.progress(percent_complete + 1, text=progress_text)
     time.sleep(1)
     my_bar.empty()
-    st.success(f"Your predicted compressive strength (MPa) of UHPC from  RF model is {(fc_pred2)}")
+    st.success(f"Your predicted incubation dose (dpa) obtained from HHO-CGB model is: {(fc_pred2)}")
 
