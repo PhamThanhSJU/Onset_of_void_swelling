6e32620a2784aba26995522b575f65d05f024f8b
PhamThanhSJU <146055328+PhamThanhSJU@users.noreply.github.com>
2025-02-25 16:48:44 +0900

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -39,37 +39,40 @@ with col1:
    X2 = st.slider("Cr (wt. %)", 0.00, 24.70)
    X3= st.slider("Mn (wt. %)", 0.00, 20.00)
    X4 = st.slider("Si (wt. %)", 0.00, 1.50)
-   X5 = st.slider("Co /100 (wt. %)", 0.00, 4.00)
+   
 	
 with col2:	
-
+   X5 = st.slider("Co /100 (wt. %)", 0.00, 4.00)
    X6= st.slider("Mo (wt. %)", 0.00, 2.95)
-   X7 = st.slider("Ni (wt. %)", 0.00, 100.00)
-   X8=st.slider("C (wt. %)", 0.00, 1.00)	
+   X8 = st.slider("C (wt. %)", 0.00, 1.00)	
    X9 = st.slider("Ti (wt. %)", 0.0, 2.20)
-   X10= st.slider("N (wt. %)", 0.00, 0.30)
+
 
 with col3:		
    X11 = st.slider("B /1000 (wt. %)", 0.00, 4.00)
    X12 = st.slider("P /100 (wt. %)", 0.00, 15.50)
    X13= st.slider("S /100 (wt. %)", 0.00, 3.00)
    X14 = st.slider("Nb (wt. %)", 0.00, 0.92)   
-   X15=st.slider("Cu (wt. %)", 0.00, 0.54)	
+   	
 	
 with col4:	
+   X15=st.slider("Cu (wt. %)", 0.00, 0.54)	
    X16 = st.slider("Ta (wt. %)", 0.00, 0.36)
    X17= st.slider("Al (wt. %)", 0.00, 100.00)
    X18 = st.slider("V (wt. %)", 0.00, 2.00)
-   X19 = st.slider("Mg (wt. %)", 0.00, 1.63)
-   X20= st.slider("W (wt. %)", 0.00, 2.40)	
+  	
 
 with col5:	
+   X19 = st.slider("Mg (wt. %)", 0.00, 1.63)
+   X20= st.slider("W (wt. %)", 0.00, 2.40)
+	
    X21 = st.slider("Zr (wt. %)", 0.00, 0.10)  
    X22=st.slider("Dose rate /1000 (dpa/s)", 0.000, 60.00)
-   X23 = st.slider("Temperature (K)", 393.5, 1013.5)
-   X24= st.slider("Irradiation type [1-4]", 1, 4)
+
 	
 with col6:	
+   X23 = st.slider("Temperature (K)", 393.5, 1013.5)
+   X24= st.slider("Irradiation type [1-4]", 1, 4)
    X25 = st.slider("Dislocation density x10^14 (m−2)", 0.30, 38.50)
    X26 = st.slider("Pre-injected He (appm)", 0.00, 100.00)
 
@@ -85,30 +88,42 @@ from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 df = pd.read_csv('Swelling_data_Thanh.csv')
-X_ori = df[['Fe','Cr','Mn','Si','Co','Mo','Ni','C','Ti','N','B','P','S','Nb','Cu','Ta','Al','V','Mg','W','Zr','dr','tem','irrtype','disden','PreHe']].values
+X_ori = df[['Fe','Cr','Mn','Si','Co','Mo','C','Ti','B','P','S','Nb','Cu','Ta','Al','V','Mg','W','Zr','dr','tem','irrtype','disden','PreHe']].values
 y = df['dose'].values
 X = scaler.fit_transform(X_ori)	
-X_train=X
-y_train=y
 
-n_estimators = 115
-learning_rate = 0.518
-depth = 9
-l2_leaf_reg = 9
-        
-cat_clf_n = CatBoostRegressor(n_estimators =n_estimators,learning_rate = learning_rate, depth=depth,l2_leaf_reg=l2_leaf_reg, random_state=42)
-cat_clf_n.fit(X_train, y_train)
-
-
-Inputdata = [X1, X2, X3, X4, X5/100, X6, X7,X8, X9, X10, X11/1000, X12/100, X13/100, X14, X15, X16, X17,X18, X19, X20, X21, X22/1000, X23, X24, X25*(10**14),X26]
-
-
+Inputdata = [X1, X2, X3, X4, X5/100, X6,X8, X9, X11/1000, X12/100, X13/100, X14, X15, X16, X17,X18, X19, X20, X21, X22/1000, X23, X24, X25*(10**14),X26]
+predictions = []
 from numpy import asarray
-Newdata1 = asarray([Inputdata])
-print(Newdata1)
-Newdata=scaler.transform(Newdata1)
+X_predict = asarray([Inputdata])
+  
+for i in range(1,2,1):
+    count = i
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=(0+i))
+    
+    n_estimators = 452 #430 #120
+    learning_rate = 0.301 #0.513
+    depth = 7
+    l2_leaf_reg = 6
+    
+    cat_clf_n = CatBoostRegressor(n_estimators=n_estimators,learning_rate = learning_rate, depth=depth,l2_leaf_reg=l2_leaf_reg, random_state=42)
+    cat_clf_n.fit(X_train, y_train)
 
-fc_pred2 = cat_clf_n.predict(Newdata)
+
+    # X_predict1= scaler.fit_transform(X_predict)
+    X_predict1=scaler.transform(X_predict)
+
+    result_xgb = cat_clf_n.predict(X_predict1)
+
+    predictions.append(result_xgb)
+
+
+mean_pred = np.mean(predictions)
+std_pred = np.std(predictions)
+
+puxgb = np.around(mean_pred, 3)
+puxgb_err = np.around(std_pred, 3)
+
 
 st.subheader("Output variable")
 if st.button("Predict"):
@@ -121,5 +136,5 @@ if st.button("Predict"):
        my_bar.progress(percent_complete + 1, text=progress_text)
     time.sleep(1)
     my_bar.empty()
-    st.success(f"Your predicted incubation dose (dpa) obtained from HHO-CGB model is: {(fc_pred2)}")
-
+    st.success(f"Your predicted incubation dose (dpa) obtained from HHO-CGB model is: {(puxgb)}")
+    st.success(f"The standard deviation (Uncertainty value) (dpa) is: {(puxgb_err)}")
